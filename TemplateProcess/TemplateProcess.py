d5638a430ae13a0444052d6ea8b342699f332858
Mike Sarahan <msarahan@gmail.com>
2014-03-07 13:16:06 -0800

    Add parameter examples to TemplateProcess

--- TemplateProcess/TemplateProcess.py
+++ TemplateProcess/TemplateProcess.py
@@ -20,15 +20,16 @@ _ = gettext.gettext
 # This should be a unique identifier for your process.  Try to be descriptive, but not generic (don't conflict with other plugins)
 script_id = _("your-process-operation")
 # this is the text that the menu will display
-process_description = _("Process operation")
+process_name = _("Process operation")
 # The prefix to prepend to the result image name:
 process_prefix = _("Your process done to ")
 
 # you can define any functions to be used in your processing here.
 # They do not need to be defined in the class itself.
 # By defining them here, you can debug them more easily by directly importing them into your testing environment.
-def your_processing_function(data, parameter1=None):
-    return data
+def your_processing_function(data, point_parameter=None, scalar_parameter=0.3, integer_parameter=1):
+    # return a copy of the data
+    return data[:]
 
 # From here down, we're using a standard layout so that Swift knows how to execute your process.
 # the most important part is the process method, which is where you'll need to add calls to your processing function(s).
@@ -38,7 +39,17 @@ class ProcessOperation(Operation.Operation):
     A Swift plugin for you to use.
     """
     def __init__(self):
-        super(ProcessOperation, self).__init__(process_description, script_id)
+        # description tells the UI what elements to create for the parameters panel.
+        # If you have no parameters, you don't need this.
+        description = [
+                    { "name": _("Point Example"), "property": "point_example", "type": "point", "default": (0.25, 0.25) },
+                    { "name": _("Scalar Example"), "property": "scalar_example", "type": "scalar", "default": 0.3 },
+                    { "name": _("Integer Example"), "property": "integer_example", "type": "integer-field", "default": 1 }
+                ]
+        # runs superclass initialization methods
+        super(ProcessOperation, self).__init__(process_name, script_id, description)
+        # if you have no parameters, just use this instead:
+        #super(ProcessOperation, self).__init__(process_name, script_id)
 
     def process(self, data):
         """
@@ -48,7 +59,13 @@ class ProcessOperation(Operation.Operation):
         This is where you'll call your methods to actually operate on the data.
         This method should always return a new copy of data
         """
-        return your_processing_function(data)
+        # a tuple
+        point_example = self.get_property("point_example")
+        # a floating point scalar
+        scalar_example = self.get_property("scalar_example")
+        # an integer
+        integer_example = self.get_property("integer_example")        
+        return your_processing_function(data, point_example, scalar_example, integer_example)
 
     def get_processed_data_shape_and_dtype(self, data_shape, data_dtype):
         """
@@ -78,7 +95,7 @@ def build_menus(document_controller):
     makes the menu entry for this plugin
     """
     operation_callback = lambda: document_controller.add_processing_operation_by_id(script_id, prefix=process_prefix)
-    document_controller.processing_menu.add_menu_item(process_description, operation_callback)
+    document_controller.processing_menu.add_menu_item(process_name, operation_callback)
 
 Application.app.register_menu_handler(build_menus) # called on import to make the menu entry for this plugin
 Operation.OperationManager().register_operation(script_id, lambda: ProcessOperation())
