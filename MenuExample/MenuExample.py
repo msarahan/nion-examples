beafd92c8a73a316dc7585770e769ef6c2080a99
Chris Meyer <cmeyer1969@gmail.com>
2014-03-10 22:06:02 -0700

    Simplify menu creation.

--- MenuExample/MenuExample.py
+++ MenuExample/MenuExample.py
@@ -32,24 +32,18 @@ def pane_command(document_controller):
 # the build_menus function will be called whenever a new document window is created.
 # it will be passed the document_controller.
 def build_menus(document_controller):
-
     # check to see if the document controller already has an example menu member.
-    if not hasattr(document_controller, "example_menu"):
-        # if not, create an example menu and assign the variable to the document controller.
-        # it will go before the "Window" menu which is known to exist.
-        document_window = document_controller.document_window
-        document_controller.example_menu = document_window.insert_menu(_("Examples"), document_controller.window_menu)
+    example_menu = document_controller.get_or_create_menu("example_menu", _("Examples"), "window_menu")
 
     # the lambda function will be called when the user chooses this menu item.
-    document_controller.example_menu.add_menu_item(_("Call Global Function..."), lambda: global_function())
+    example_menu.add_menu_item(_("Call Global Function..."), lambda: global_function())
 
     # the lambda function passes document controller from this scope.
-    document_controller.example_menu.add_menu_item(_("Call Document Controller Command..."),
-                                                   lambda: document_controller_command(document_controller))
+    example_menu.add_menu_item(_("Call Document Controller Command..."),
+                               lambda: document_controller_command(document_controller))
 
     # the lambda function passes document controller from this scope.
-    document_controller.example_menu.add_menu_item(_("Call Data Pane Command..."),
-                                                   lambda: pane_command(document_controller))
+    example_menu.add_menu_item(_("Call Data Pane Command..."), lambda: pane_command(document_controller))
 
 
 # register the menu handler with the application.
